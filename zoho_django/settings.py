1c7aad084645d2d1a9b85d0436e5fdad705b5c0f
kozhukkatta <pauljogeorge45@gmail.com>
2023-11-03 21:45:16 +0530

    second commit

--- zoho_django/settings.py
+++ zoho_django/settings.py
@@ -86,8 +86,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'infoxtec_zohodb',
-        'USER':'infoxtec_zohodb',
-        'PASSWORD':'zohodbzohodb',
+        'USER':'root',
+        'PASSWORD':'',
         'HOST':'localhost',
         'PORT':'3306',
     }
